90dd5997080bc7b00cbbb9dc3fbb34e048df6356
YNTan21 <yunning21@gmail.com>
2024-09-10 23:42:12 +0800

    add file

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -96,7 +96,7 @@
             <div class="p-4 sm:ml-64">
                 <div class="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-14">
                 
-                
+                <h1>dashboard</h1>
                 </div>
             </div>
         </div>
